0334d20e3d653b5148c65e37aa85d283b082a049
rmilano24 <rmilano24@gmail.com>
2017-10-31 13:30:04 -0700

    update path to css

--- wp-content/themes/milano/header.php
+++ wp-content/themes/milano/header.php
@@ -16,7 +16,7 @@
 		<?php endif; ?>
 
 		<?php wp_head(); ?>
-		<link rel='stylesheet' id='agncy-style-css'  href='http://milanoagency.wpengine.com/agency/wp-content/themes/agncy/milano.css' type='text/css' media='all' />
+		<link rel='stylesheet' id='agncy-style-css'  href='http://milanoagency.wpengine.com/wp-content/themes/agncy/milano.css' type='text/css' media='all' />
 	</head>
 
 	<body <?php body_class(); ?>>
