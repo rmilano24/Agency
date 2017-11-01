863b89021b808160ad3732a13d9d48c185ec6c38
rmilano24 <rmilano24@gmail.com>
2017-11-01 09:43:53 -0700

    update favicon links

--- wp-content/themes/milano/header.php
+++ wp-content/themes/milano/header.php
@@ -17,11 +17,11 @@
 
 		<?php wp_head(); ?>
 		<link rel='stylesheet' id='agncy-style-css'  href='https://designbymilano.com/wp-content/themes/milano/milano.css' type='text/css' media='all' />
-		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-		<link rel="manifest" href="/manifest.json">
-		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="apple-touch-icon" sizes="180x180" href="https://designbymilano.com/wp-content/themes/milano//apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="https://designbymilano.com/wp-content/themes/milano//favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="https://designbymilano.com/wp-content/themes/milano//favicon-16x16.png">
+		<link rel="manifest" href="https://designbymilano.com/wp-content/themes/milano//manifest.json">
+		<link rel="mask-icon" href="https://designbymilano.com/wp-content/themes/milano//safari-pinned-tab.svg" color="#5bbad5">
 		<meta name="theme-color" content="#ffffff">
 	</head>
 
