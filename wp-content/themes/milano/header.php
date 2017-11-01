592796aea64d33739d0b146f1e309e9956116a9a
rmilano24 <rmilano24@gmail.com>
2017-11-01 09:41:58 -0700

    add favicons

--- wp-content/themes/milano/header.php
+++ wp-content/themes/milano/header.php
@@ -17,6 +17,12 @@
 
 		<?php wp_head(); ?>
 		<link rel='stylesheet' id='agncy-style-css'  href='https://designbymilano.com/wp-content/themes/milano/milano.css' type='text/css' media='all' />
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/manifest.json">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="theme-color" content="#ffffff">
 	</head>
 
 	<body <?php body_class(); ?>>
